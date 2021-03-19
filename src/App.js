d6ec66f49a6a229fbe1b9c42e4e3966a9453457a
RushiVachhani <vachhanirushi30@gmail.com>
2021-03-19 20:27:50 +0530

    completed hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Navbar></Navbar>
         <Switch>
-          <Route path='/' exact></Route>
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default Home;
